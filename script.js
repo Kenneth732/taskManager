7b5b6aef948ffe2cd94c42d5e804a05ec8efb645
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-30 11:41:32 +0300

    readme

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ let taskManager = []
 document.getElementById('form').addEventListener('submit', (e) => {
     e.preventDefault();
 
+    let taskInput = e.target.taskInput.value;
+
     if(taskInput.trim() === ''){
-        alert(``)
+        alert(`Please enter Task`)
+    }return;
+
+    let newTask = {
+        taskInput: taskInput
     }
-})
+    taskManager.push(newTask);
+    handleDisplay(taskManager);
+});
+
+let handleDisplay = (tasks) => {
+    
+}
