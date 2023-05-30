7361ea2cb8f611ea7f8d514cb06ecca8cbfd9cfb
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-30 11:36:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let taskManager = []
+
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if(taskInput.trim() === ''){
+        alert(``)
+    }
+})
